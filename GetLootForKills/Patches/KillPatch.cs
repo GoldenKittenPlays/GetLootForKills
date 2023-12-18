9dabf8b137df4fae9057a9b21dca0d1fcf0c02e5
GoldenKitten <134951434+GoldenKittenPlays@users.noreply.github.com>
2023-12-18 04:43:59 -0800

    Add project files.

---
+++ GetLootForKills/Patches/KillPatch.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace GetLootForKills.Patches
+{
+    [HarmonyPatch(typeof(EnemyAI))]
+    internal class KillPatch
+    {
+
+        [HarmonyPatch("KillEnemyOnOwnerClient")]
+        [HarmonyPostfix]
+        static void patchKillEnemyOnOwnerClient(ref EnemyAI __instance)
+        {
+            if (__instance.isEnemyDead)
+            {
+                RoundManager instance = RoundManager.Instance;
+                int itemID = UnityEngine.Random.Range(0, instance.currentLevel.spawnableScrap.Count);
+                Item item = instance.currentLevel.spawnableScrap[itemID].spawnableItem;
+                Vector3 position = __instance.transform.position + Vector3.up * 0.6f;
+                position += new Vector3(UnityEngine.Random.Range(-0.8f, 0.8f), 0f, UnityEngine.Random.Range(-0.8f, 0.8f));
+                GameObject obj = UnityEngine.Object.Instantiate(item.spawnPrefab, position, Quaternion.identity, RoundManager.Instance.spawnedScrapContainer);
+                obj.GetComponent<GrabbableObject>().fallTime = 0f;
+                obj.GetComponent<NetworkObject>().Spawn();
+            }
+        }
+    }
+}
---
+++ GetLootForKills/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using GetLootForKills.Patches;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetLootForKills
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string modGUID = "GoldenKitten.GetLootForKills", modName = "GetLootForKills", modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static Plugin Instance;
+
+        internal ManualLogSource logger;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            logger = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            logger.LogInfo("GetLootForKills initiated!");
+
+            harmony.PatchAll(typeof(Plugin));
+            harmony.PatchAll(typeof(KillPatch));
+        }
+    }
+}
